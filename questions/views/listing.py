790102e2f7b93289b537a237e6def7c50c336d10
Suetone Carneiro <suetoneneto12@gmail.com>
2026-09-23 16:05:55 -0300

    feat(questions): RF4 role-based question list

--- questions/views/listing.py
+++ questions/views/listing.py
@@ -1,8 +1,35 @@
-from django.http import HttpResponse
-from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+
+from ..models import Question
 
 
-class QuestionListView(View):
-    # TODO(Suetone): RF4, LoginRequiredMixin + ListView using Question.objects.visible_to(user)
-    def get(self, request):
-        return HttpResponse('TODO: question list')
+class QuestionListView(LoginRequiredMixin, ListView):
+    """RF4: one page, three different result sets depending on who opens it."""
+
+    model = Question
+    template_name = 'questions/question_list.html'
+    context_object_name = 'questions'
+
+    def get_queryset(self):
+        return (
+            Question.objects.visible_to(self.request.user)
+            .select_related('subject', 'author', 'assigned_monitor')
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        monitored_subjects = user.monitored_subjects.all()
+
+        if user.has_perm('questions.view_all_questions'):
+            context['heading'] = 'All questions'
+            context['subheading'] = 'Every question from every subject.'
+        elif monitored_subjects:
+            codes = ', '.join(subject.code for subject in monitored_subjects)
+            context['heading'] = 'Questions in your subjects'
+            context['subheading'] = f'You monitor {codes}. Your own questions show up here too.'
+        else:
+            context['heading'] = 'My questions'
+            context['subheading'] = 'Only you and the monitors of each subject can see these.'
+        return context
